2430103f9991b06d43c77e0a4ad9744ff625f97d
babith1234 <you@example.com>
2024-01-02 20:54:55 +0530

    database connected

---
+++ src/app/dbConfig/dbConfig.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export async function connect() {
+    try {
+      const mongodbUri = process.env.MONGODB_URI;
+      console.log(mongodbUri);
+  
+      if (!mongodbUri) {
+        console.log("unknown mongodb uri");
+      }
+  
+      await mongoose.connect(mongodbUri);
+  
+      console.log("Connected to MongoDB!");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+
